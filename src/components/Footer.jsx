d1904fe2f673fadfc3ffe7ecd75d3b60bd68e448
janunezdevelopment <157073673+janunezdevelopment@users.noreply.github.com>
2026-07-09 17:13:34 -0500

    feat: enhance footer layout and styling for improved visual hierarchy and responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,18 +17,21 @@ function Footer() {
 
   return (
     <footer>
-      <h3>Socials</h3>
-      <nav>
-        <ul className="social-links-container">
-          {socialLinks.map((link, index) => (
-            <li key={index}>
-              <a href={link.href} target="_blank" rel="noopener noreferrer">
-                <img src={link.src} alt={link.label} />
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <div className="social-section-container">
+        <h3>Socials</h3>
+        <nav>
+          <ul className="social-links-container">
+            {socialLinks.map((link, index) => (
+              <li key={index}>
+                <a href={link.href} target="_blank" rel="noopener noreferrer">
+                  <img src={link.src} alt={link.label} />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
       <p className="copyright">
         &copy; 2026 Javier A. Núñez.
         <br />
